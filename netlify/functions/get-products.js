d25483b7366f6fd2f4c9810a0bfe68d006b39898
MauricioDiaz-G <mauriciousuario122@gmail.com>
2025-01-18 22:25:08 +0100

    Update get-products.js

--- netlify/functions/get-products.js
+++ netlify/functions/get-products.js
@@ -2,17 +2,22 @@ const stripe = require('stripe')(process.env.stripeSecretKeyARON); // Usar la cl
 
 exports.handler = async function (event, context) {
     try {
+        // Obtener productos y precios desde Stripe
         const productos = await stripe.products.list();
         const precios = await stripe.prices.list();
 
+        // Retornar los datos en formato JSON
         return {
             statusCode: 200,
-            body: JSON.stringify({ productos, precios }),  // Asegúrate de que esto sea JSON
+            body: JSON.stringify({ productos: productos.data, precios: precios.data }),  // Asegúrate de que esto sea JSON
         };
     } catch (error) {
+        // Manejo de errores
         return {
             statusCode: 500,
             body: JSON.stringify({ error: error.message }),  // Esto debe devolver un JSON con el error
         };
     }
 };
+
+
