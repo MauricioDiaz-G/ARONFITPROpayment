fe4654d079a347fbdd98e979f34838477596a798
MauricioDiaz-G <mauriciousuario122@gmail.com>
2025-01-18 21:07:32 +0100

    Update keys.js

--- assets/keys.js
+++ assets/keys.js
@@ -1,4 +1,4 @@
 export default {
     public: "pk_test_51QXTdzDleD0jqpl3oUTZiW3tdVjZzSG3JH8TGy1ghUPsNntgblBgRlHbWZMZwUvA9OOs2qhiP6f3O0BpEHS9MXK600yVZ49SnI",
-    // secret: "sk_test_51QXTdzDleD0jqpl3WupiZiemOPeW6RlonnVcpVnMm5rPXvVpdn29n1VlZc3PgPpRXdnyMi4rjx1QvUMiLpmFHOAZ00Wqe0YpCI"
+  
   }
